7d9af8a787f5b89dc9f91112e83371a8d992ddb9
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-01 16:41:37 +0100

    Use `flatMap` to simplify `register` method logic.

--- backend/app/service/RegistrationService.scala
+++ backend/app/service/RegistrationService.scala
@@ -6,24 +6,19 @@ import repository.UserRepository
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Success
 
 @Singleton
 class RegistrationService @Inject()(repository: UserRepository)(implicit ec: ExecutionContext) {
 
   def register(userCreate: UserCreate): Future[Either[String, UserResponse]] = {
-    repository.findByUsername(userCreate.username).map(userOption => {
-      userOption match {
-        case Some(existing) => Left(s"Username [${existing.username}] already exists: ")
-        case None => {
-          println(s"About to create user: $userCreate")
-          val user = User(username = userCreate.username, passwordHash = "<PASSWORD>", name = userCreate.name) // TODO hash the password
-          val eventualUser = repository.create(user)
-          eventualUser.map {
-            createdUser => Right(UserResponse(createdUser.id, createdUser.username, createdUser.name))
-          }
+    repository.findByUsername(userCreate.username).flatMap {
+      case Some(existing) => Future.successful(Left(s"Username [${existing.username}] already exists"))
+      case None =>
+        println(s"About to create user: $userCreate")
+        val user = User(username = userCreate.username, passwordHash = "<PASSWORD>", name = userCreate.name) // TODO hash the password
+        repository.create(user).map {
+          createdUser => Right(UserResponse(createdUser.id, createdUser.username, createdUser.name))
         }
-      }
-    })
+    }
   }
 }
