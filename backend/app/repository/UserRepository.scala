34d7a5297c8d3542fdd319ad20b6bf4c0ba275fd
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-01 14:39:43 +0100

    Introduce UserRepository

---
+++ backend/app/repository/UserRepository.scala
@@ -0,0 +1,10 @@
+package repository
+
+import slick.jdbc.MySQLProfile.api._
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class UserRepository @Inject()(db: Database) {
+
+}
--- backend/app/service/RegistrationService.scala
+++ backend/app/service/RegistrationService.scala
@@ -1,12 +1,13 @@
 package service
 
 import dto.{UserCreate, UserResponse}
+import repository.UserRepository
 
-import javax.inject.Singleton
+import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
 
 @Singleton
-class RegistrationService {
+class RegistrationService @Inject()(repository: UserRepository){
 
   def register(user: UserCreate): Future[Either[String, UserResponse]] = {
     Future.successful(Right(UserResponse(id = 1, username = user.username, name = "MOCK USER from Service")))
