9801c2a59c3a56bdb74a445128cfe1b255c00a4a
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-03 10:13:13 +0100

    Add Home page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import {Navigate, Route, Routes} from "react-router-dom";
+
 import Register from './pages/Register'
 import Login from "./pages/Login.tsx";
+import Home from "./pages/Home.tsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
       <Route path="*" element={<Navigate to="/login" replace/>}/>
     </Routes>
   )
