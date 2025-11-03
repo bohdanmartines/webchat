5b79d696542a43f28e9fbdeb12a1ec0dc26584d1
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-03 10:21:52 +0100

    Make Home page protected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,21 @@ import Register from './pages/Register'
 import Login from "./pages/Login.tsx";
 import Home from "./pages/Home.tsx";
 
+function ProtectedPage({children}: { children: JSX.Element }) {
+  return children;
+}
+
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/home" element={<Home/>}/>
+      <Route path="/home"
+             element={
+               <ProtectedPage>
+                 <Home/>
+               </ProtectedPage>
+             }/>
       <Route path="*" element={<Navigate to="/login" replace/>}/>
     </Routes>
   )
