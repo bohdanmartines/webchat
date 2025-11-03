5e9dd0891f56541b103abc1d67916b057e951736
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-03 08:54:41 +0100

    Add Register page component. v2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
-import {Navigate, Route} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Register from './pages/Register'
 
 function App() {
   return (
-      <div className="min-h-screen flex flex-col">
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/register" replace />} />
-      </div>
+    <Routes>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="*" element={<Navigate to="/register" replace/>}/>
+    </Routes>
   )
 }
 
