2f861eda9e4c4894712481bc77865bd609dae467
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-03 08:57:51 +0100

    Add Login page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Navigate, Route, Routes} from "react-router-dom";
 import Register from './pages/Register'
+import Login from "./pages/Login.tsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/register" element={<Register/>}/>
-      <Route path="*" element={<Navigate to="/register" replace/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="*" element={<Navigate to="/login" replace/>}/>
     </Routes>
   )
 }
