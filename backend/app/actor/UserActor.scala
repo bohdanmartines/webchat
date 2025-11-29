4b13410fff9442a5c79b12a09a67735b47c7d4ac
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-29 15:43:57 +0100

    Add skeleton for authentication handling to `UserActor` class

--- backend/app/actor/UserActor.scala
+++ backend/app/actor/UserActor.scala
@@ -7,15 +7,26 @@ import play.api.libs.json.JsValue
 import scala.concurrent.ExecutionContext
 
 class UserActor extends Actor {
+
+  private var authenticated = false
+
   override def receive: Receive = {
     case msg: JsValue => {
       parseClientMessage(msg) match {
-        case Some(Authenticate(_)) => println(s"Received an authenticate message")
+        case Some(Authenticate(token)) if !authenticated =>
+          println(s"Received an authenticate message")
+          handleAuthentication(token)
         case Some(SendMessage(content)) => println(s"Received a client message '$content'")
         case None => println("Unknown message format")
       }
     }
     case _ => println("Unknown message format")
+  }
+
+  private def handleAuthentication(token: String) = {
+    println("Authenticating a user")
+    // TODO Verify the token
+    authenticated = true
   }
 }
 
