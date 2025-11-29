f2e218239365672b6060a467453140f39e35f069
Bohdan Martines <bohdan.explorer@gmail.com>
2025-11-29 14:17:20 +0100

    Add message handling logic skeleton to `UserActor` class

--- backend/app/actor/UserActor.scala
+++ backend/app/actor/UserActor.scala
@@ -1,10 +1,18 @@
 package actor
 
-import org.apache.pekko.actor.Props
+import org.apache.pekko.actor.{Actor, Props}
+import play.api.libs.json.JsValue
 
 import scala.concurrent.ExecutionContext
 
-class UserActor {}
+class UserActor extends Actor {
+  override def receive: Receive = {
+    case msg: JsValue => {
+      println(msg)
+    }
+    case _ => println("Unknown message")
+  }
+}
 
 object UserActor {
   def props()(implicit ec: ExecutionContext): Props = {
