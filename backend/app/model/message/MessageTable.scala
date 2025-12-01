250c607be37708f96bc8bc30286a4e3c75f39ea1
Bohdan Martines <bohdan.explorer@gmail.com>
2025-12-01 22:48:05 +0100

    Fix column name

--- backend/app/model/message/MessageTable.scala
+++ backend/app/model/message/MessageTable.scala
@@ -8,7 +8,7 @@ import java.time.LocalDateTime
 class MessageTable (tag: Tag) extends Table[Message](tag, "messages") {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def chatId = column[Long]("chat_id")
-  def userId = column[Long]("userId")
+  def userId = column[Long]("user_id")
   def content = column[String]("content")
   def createdAt = column[LocalDateTime]("created_at")
 
