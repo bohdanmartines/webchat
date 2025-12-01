cf0d668b449a97baf39a59249addab08f27944d0
Bohdan Martines <bohdan.explorer@gmail.com>
2025-12-01 21:49:27 +0100

    Add MessageRepository

---
+++ backend/app/repository/MessageRepository.scala
@@ -0,0 +1,23 @@
+package repository
+
+import model.message.{Message, MessageTable}
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+
+import javax.inject._
+import scala.concurrent.Future
+
+@Singleton
+class MessageRepository @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
+                                 (implicit ec: scala.concurrent.ExecutionContext) {
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
+  import dbConfig._
+  import profile.api._
+
+  private val messages = TableQuery[MessageTable]
+
+  def create(messageToInsert: Message): Future[Message] = {
+    val insert = (messages returning messages.map(_.id) into ((message, id) => message.copy(id = id))) += messageToInsert
+    db.run(insert)
+  }
+}
