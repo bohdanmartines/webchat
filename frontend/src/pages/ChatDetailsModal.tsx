4b1d1a1a67a56f376ff9f050adfbc83eb088514e
Bohdan Martines <bohdan.explorer@gmail.com>
2025-12-24 07:55:51 +0100

    List participants

--- frontend/src/pages/ChatDetailsModal.tsx
+++ frontend/src/pages/ChatDetailsModal.tsx
@@ -1,7 +1,8 @@
-import '../css/CreateChatModal.css';
+import '../css/ChatDetails.css';
 import {getChatDisplayName} from "../types/Chat.ts";
+import type {User} from "../types/User.ts";
 
-function ChatDetailsModal({ chat, isOpen, onClose }) {
+function ChatDetailsModal({chat, isOpen, onClose}) {
 
   function handleClose() {
     onClose();
@@ -9,12 +10,28 @@ function ChatDetailsModal({ chat, isOpen, onClose }) {
 
   if (!isOpen) return null;
 
-  return(
+  return (
     <div className="modal-backdrop">
       <div className="modal-content">
         <div className="modal-header">
           <span className="chat-title">{getChatDisplayName(chat)}</span>
           <button className="close-btn" onClick={handleClose}>×</button>
+        </div>
+        <div className="modal-body">
+          <p>{chat.participants.length} participant(s)</p>
+
+          <p>Add a participant</p>
+          <input type="text" placeholder="Participant username"/>
+
+          {
+            chat.participants.map((participant: User) =>
+              <div
+                key={participant.id}
+                className="participant-item">
+                <p>{participant.username}</p>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
